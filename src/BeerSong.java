1c44a936ad5e582c37d574f28bb6a702227b7946
Michael Piotrowski <mlp95747@gmail.com>
2022-07-21 11:33:12 -0700

    From the book Head First Java For my private preactice. July 21, 2022. Done in Intellij IDEA 2022

---
+++ src/BeerSong.java
@@ -0,0 +1,36 @@
+// July 21, 2022 from the Book Headfirst Java
+// This will be kept private for my learning purposes
+
+
+
+public class BeerSong {
+
+    public static void main (String[] arges){
+
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0){
+
+
+            if (beerNum == 1){
+                word = "bottle"; // singular, as in ONE bottle
+            }
+
+            System.out.println("beerNum + "  + word +  " of beer on the wall");
+            System.out.println("beerNum + "  + word +  " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum -1;
+
+            if (beerNum == 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall.");
+            }
+                else{
+                    System.out.println("No more bottles of beer on the wall.");
+                } // End else
+
+        } // End while loop
+
+    }//End main method
+} // end class
